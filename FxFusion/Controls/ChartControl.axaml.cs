e1fb3d2ac06a76d877990d1a607accf149ca41bf
Artur Durasiewicz <artur@durasiewi.cz>
2023-12-27 18:44:07 +0100

    Chart render infrastructure.

---
+++ FxFusion/Controls/ChartControl.axaml.cs
@@ -0,0 +1,54 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Platform;
+using Avalonia.Rendering.SceneGraph;
+using Avalonia.Skia;
+using Avalonia.Threading;
+using SkiaSharp;
+
+namespace FxFusion.Controls;
+
+public partial class ChartControl : UserControl
+{
+    private readonly ChartDrawOperation _chartDrawOperation;
+
+    public ChartControl()
+    {
+        InitializeComponent();
+        ClipToBounds = true;
+        _chartDrawOperation = new ChartDrawOperation();
+    }
+
+    private class ChartDrawOperation : ICustomDrawOperation
+    {
+        public void Dispose() { }
+        public void BeginRender(Rect bounds) => Bounds = bounds;
+        public Rect Bounds { get; private set; }
+        public bool HitTest(Point p) => false;
+        public bool Equals(ICustomDrawOperation? other) => false;
+
+        public void Render(ImmediateDrawingContext context)
+        {
+            var leaseFeature = context.TryGetFeature<ISkiaSharpApiLeaseFeature>();
+            
+            if (leaseFeature is null)
+            {
+                return;
+            }
+
+            using var lease = leaseFeature.Lease();
+            var canvas = lease.SkCanvas;
+            canvas.Save();
+            canvas.Clear(SKColors.Moccasin);
+            canvas.Restore();
+        }
+    }
+    
+    public override void Render(DrawingContext context)
+    {
+        _chartDrawOperation.BeginRender(new Rect(0, 0, Bounds.Width, Bounds.Height));
+        context.Custom(_chartDrawOperation);
+        Dispatcher.UIThread.InvokeAsync(InvalidateVisual, DispatcherPriority.Background);
+    }
+}
